acc3159482bc2fdd33105fd7bc3b9c5e385fe362
dfgdfdfgsdfe <yiheewoo04@gmail.com>
2026-05-22 22:58:16 +0900

    Update renderer.py

--- ui/renderer.py
+++ ui/renderer.py
@@ -1,1 +1,28 @@
+import matplotlib.pyplot as plt
 
+from engine.board import BLACK, WHITE
+
+
+STAR_POINTS = [
+    (3, 3),
+    (3, 9),
+    (3, 15),
+    (9, 3),
+    (9, 9),
+    (9, 15),
+    (15, 3),
+    (15, 9),
+    (15, 15),
+]
+
+
+def render_board(board):
+    fig, ax = plt.subplots(figsize=(9, 9))
+
+    ax.set_facecolor('#DEB887')
+
+    for i in range(board.size):
+        ax.plot([0, 18], [i, i], color='black')
+        ax.plot([i, i], [0, 18], color='black')
+
+    for x
