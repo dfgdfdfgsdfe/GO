828175cf616d365539075e72a3e969d1a6b05469
dfgdfdfgsdfe <yiheewoo04@gmail.com>
2026-05-22 22:58:00 +0900

    Update ai.py

--- engine/ai.py
+++ engine/ai.py
@@ -1,1 +1,13 @@
+from engine.mcts import MCTS
 
+
+class GoAI:
+    def __init__(self):
+        self.mcts = MCTS(
+            min_time=0,
+            max_time=20,
+            rollout_depth=60
+        )
+
+    def select_move(self, board):
+        return self.mcts.search(board)
